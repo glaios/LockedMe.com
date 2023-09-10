48b05dbce5c54bb5a7db676b1a4fe0442123429e
Laios Giannis <laiosgiannis@gmail.com>
2023-09-10 10:51:11 +0300

    documents

--- src/locked/me/com/mainmenu/MainMenu.java
+++ src/locked/me/com/mainmenu/MainMenu.java
@@ -5,7 +5,7 @@ public class MainMenu {
 
 	public static void MainMenu() {
 	    System.out.println("LockedMe.com App");
-	    System.out.println("By Giannis Laios - laiosgiannis@gmail.com");
+	    System.out.println("By Giannis Laios - ioannis.laios@vodafone.com");
 	    System.out.println("1. If you would like to view the list of files stored!");
 	    System.out.println("2. If you would like to manage the files (Add,Delete and Search)!");
 	    System.out.println("3. If you would like to Exit the App");
